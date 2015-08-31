5d35bcf374a76f35b5acb6be9c413979c26ca366
8adam95 <adam.szefer@gmail.com>
2015-08-31 14:38:32 -0700

    counting from left then looking from left we take most right place which is OK

--- wieza2.cpp
+++ wieza2.cpp
@@ -18,8 +18,35 @@ using namespace std;
 #define MP make_pair
 #define PB push_back
 
+const int MAXN = 500005;
+
+int hi[MAXN];
+
 int main()
 {
-
+  int n, m;
+  scanf("%d %d", &n, &m);
+  
+  FOR(i, 1, n)
+  {
+    int a;
+    scanf("%d", &a), hi[i] = max(hi[i-1], a);
+  }
+  
+  int end = n;
+  
+  while(m--)
+  {
+    int b;
+    scanf("%d", &b);
+    while(end >= 0 && hi[end] >= b)
+      end--;
+    end = max(end, 0);
+    hi[end] = 1000000009;
+    printf("%d ", end);
+  }
+  
+  printf("\n");
+  
   return 0;
 }
