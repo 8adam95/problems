27461b4c7a58faf8eed0a8d67e71adb3dac786e5
8adam95 <adam.szefer@gmail.com>
2015-08-31 14:27:30 -0700

    scheme

---
+++ wieza2.cpp
@@ -0,0 +1,25 @@
+//http://main.edu.pl/pl/archive/ilocamp/2010/wiz
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<map>
+#include<cstring>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+int main()
+{
+
+  return 0;
+}
