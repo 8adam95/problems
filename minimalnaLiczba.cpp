19fc7c6a29305a9f5b7b40c3d43727c537aa9c84
8adam95 <adam.szefer@gmail.com>
2015-08-31 17:23:46 -0700

    akwarium to finish! minimalna with small bug

---
+++ minimalnaLiczba.cpp
@@ -0,0 +1,45 @@
+//http://main.edu.pl/pl/archive/oig/2/min
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<map>
+#include<cstring>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+const int MAXN = 1000006;
+long long a, k;
+int n, he = 0;
+
+vector<long long> B;
+
+int main()
+{
+  scanf("%d %lld", &n, &k);
+  REP(i, n)
+  {
+    ll a;
+    scanf("%lld", &a);
+    if(a % k == 0)
+      B.push_back(a), he++;
+  }
+    
+  sort(B.begin(), B.end());
+  
+  REP(i, he)
+    if((ll)(i+1)*k != B[i]) 
+      return printf("%lld\n", (ll)(i+1)*k);
+  
+  return 0;
+}
