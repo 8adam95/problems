5004611a499476982f6de3a147344e1f0f236c4c
8adam95 <adam.szefer@gmail.com>
2015-09-03 17:44:43 -0700

    scheme

---
+++ ratownik.cpp
@@ -0,0 +1,28 @@
+//http://main.edu.pl/pl/archive/oig/6/rat
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+
+
+int main()
+{
+  int n, k, x, y;
+  
+  return 0;
+}
