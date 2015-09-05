6501ec18b18a32a468c11c62789b0665c90f8733
8adam95 <adam.szefer@gmail.com>
2015-09-04 17:11:46 -0700

    silownia cheme

---
+++ silownia.cpp
@@ -0,0 +1,16 @@
+//http://main.edu.pl/en/archive/ilocamp/2010/sil
+
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
