6694e13c6f559a397ceffbfb519d63d083f7c3fe
8adam95 <adam.szefer@gmail.com>
2015-09-09 01:25:54 +0200

    codeforces 1

---
+++ lalaLand.cpp
@@ -0,0 +1,25 @@
+//http://codeforces.com/contest/558/problem/A
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<map>
+#include<vector>
+#include<set>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+
+int main()
+{
+
+  return 0;
+}
