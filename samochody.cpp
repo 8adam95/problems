ee3ed47b8083651674d4e887a00ce06e813c14cd
8adam95 <adam.szefer@gmail.com>
2015-09-10 01:52:44 +0200

    scheme

---
+++ samochody.cpp
@@ -0,0 +1,29 @@
+//http://main.edu.pl/pl/archive/ilocamp/2011/sam
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<map>
+#include<vector>
+#include<set>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+int main()
+{
+  scanf("%d", &n);
+  FOR(i, 1, n)
+    scanf("%d", &tab[i]);
+    
+    
+  
+  return 0;
+}
