1e0a33fcafefd00a67f21cd3df0a9e769fa64dff
8adam95 <adam.szefer@gmail.com>
2015-09-11 23:27:12 +0200

    sticks solution

--- gameWithSticks.cpp
+++ gameWithSticks.cpp
@@ -23,11 +23,12 @@ int main()
 {
   int n, m;
   scanf("%d %d", &n, &m);
-  
-  if((n*m) % 2 == 1)
-    printf("Akshat\n");
-  else
+  if(n > m)
+    swap(n, m);
+  if(n%2 == 0)
     printf("Malvika\n");
+  else
+    printf("Akshat\n");
   
   return 0;
 }
