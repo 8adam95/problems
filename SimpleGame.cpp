164e161a34ac711a29ca6891d82cda59dd8e3609
8adam95 <adam.szefer@gmail.com>
2015-09-20 20:15:47 +0100

    scheme

---
+++ SimpleGame.cpp
@@ -0,0 +1,19 @@
+//http://codeforces.com/problemset/problem/570/B
+
+#include<cstdio>
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
