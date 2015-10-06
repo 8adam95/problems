2531ee26fef88d8f7dd1af7d3ce9c1759828f12f
8adam95 <adam.szefer@gmail.com>
2015-10-07 00:29:42 +0100

    problem B

--- suffixStructures.cpp
+++ suffixStructures.cpp
@@ -19,20 +19,51 @@ using namespace std;
 
 string s, t;
 
+int tab1[105], tab2[105];
+
 int main()
 {
   cin >> s >> t;
 
-  string a = s;
-  string b = t;
-  
   int n1 = s.length();
   int n2 = t.length();
+
+  REP(i, n1)
+    tab1[s[i]-'a']++;
+    
+  REP(i, n2)
+    tab2[t[i]-'a']++;
   
+    
+  bool a = true, b = true;
   
-  sort(a, a.length());
-  sort(b, b.length());
+  REP(i, 26)
+    if(tab1[i] != tab2[i])
+      a = false;
+    
+  int j = 0;
+  REP(i, n2)
+  {
+    while(j <= n1 && s[j] != t[i])
+      j++;
+    if(j > n1)
+      b = false;
+    j++;
+  }
+  if(a)
+    return printf("array\n"), 0;
+  if(b)
+    return printf("automaton\n"), 0;
   
+  a = true;
+  REP(i, 26)
+    if(tab1[i] < tab2[i])
+      a = false;
+    
+  if(a)
+    printf("both\n");
+  else
+    printf("need tree\n");
   
   
   
