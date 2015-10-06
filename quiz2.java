41802ebcfcc6855d0bddd39724dce47572dd403c
8adam95 <adam.szefer@gmail.com>
2015-10-07 00:08:38 +0100

    java quiz

---
+++ quiz2.java
@@ -0,0 +1,16 @@
+public class quiz2
+{
+  public static void main(String args[])
+  {
+  int total = 0;
+for(int i = 0;i < 10;++i)
+{
+  ++total;
+  while(i < total)
+  {
+    ++i;
+  }
+}
+System.out.println(total);
+  }
+}
---
+++ suffixStructures.cpp
@@ -0,0 +1,40 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<cstring>
+#include<list>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+string s, t;
+
+int main()
+{
+  cin >> s >> t;
+
+  string a = s;
+  string b = t;
+  
+  int n1 = s.length();
+  int n2 = t.length();
+  
+  
+  sort(a, a.length());
+  sort(b, b.length());
+  
+  
+  
+  
+  return 0;
+}
