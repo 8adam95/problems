152572b41255d100f0e96b4a1acdc3319201b766
8adam95 <adam.szefer@gmail.com>
2015-10-15 23:09:43 +0100

    java program

---
+++ WorkingWithNumbers.java
@@ -0,0 +1,14 @@
+public class WorkingWithNumbers
+{
+  public static void main(String[] args)
+  {
+    double oneMillion = 1000000.0;
+    double mystery = oneMillion*oneMillion;
+    
+    double unitPrice = 4.35;
+    double totalPrice = 100*unitPrice;
+    System.out.println(totalPrice);
+
+  }
+
+}
