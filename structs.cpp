8f532d7b2638adc31355af3bb302cf39c92856e4
8adam95 <adam.szefer@gmail.com>
2015-10-15 22:43:46 +0100

    struct

---
+++ structs.cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+#define REP(I, N) for(int I = 0; I < (N); I++)
+#define FOR(I, A, B) for(int I = (A); I <= (B); I++)
+#define FORD(I, A, B) for(int I = (A); I >= (B); I--)
+#define ll long long
+#define F first
+#define S second
+#define MP make_pair
+#define PB push_back
+
+int main()
+{
+  int a, d;
+  string b, c;
+  scanf("%d", &a);
+  cin >> b;
+  cin >> c;
+  cin >> d;
+  cout << a << " " << b << " " << c << " " << d << "\n";
+
+
+  return 0;
+}
